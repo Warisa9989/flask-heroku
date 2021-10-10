84baeebe0965a58cdedd45e5af21945b580c088a
Warisa9989 <89174813+Warisa9989@users.noreply.github.com>
2021-10-11 00:08:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -34,9 +34,9 @@ def get_api():
     return jsonify(data)
 
 
-@app.route(‘/hi’)
-@app.route(‘/hello’)
+@app.route('/hi')
+@app.route('/hello')
 def hi():
-   return “สวัสดี”     
+   return "สวัสดี"     
 if __name__ == "__main__":
     app.run(debug=False)
