00bceb11602ebfc7aa041354d06ddf6c694c2fbb
Warisa9989 <89174813+Warisa9989@users.noreply.github.com>
2021-10-10 23:33:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ data = [
 
 @app.route('/')
 def hello():
-    return "Hello Flask-Herok" 
+    return "Hello วริษา ริมสมุทร เลขที่37 ห้อง4/5" 
 
 @app.route('/api', methods=['GET'])
 def get_api():
